9bdc4480ac87afa6c2dd642d090a3c26af5d9480
Crazy120 <80100183+Crazy120@users.noreply.github.com>
2022-05-15 12:33:59 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+from pyrogram import Client, filters
+from os import environ
+
+app = Client("AutoForwarder", api_id=int(environ["API_ID"]), api_hash=environ["API_HASH"],
+             phone_number=environ["PHONE_NUMBER"],
+             session_string=environ["SESSION_STRING"]
+             )
+
+@app.on_message(filters.regex(".start"))
+async def start(app, msg):
+    await msg.reply("<b>Hello I'm A Auto Join Request Accept Bot\n\nMade In India :) By @Walker_web</b>")
+
+@app.on_message(filters.regex(".run"))
+async def run(app, msg):
+    await msg.reply("<b>Accepting All Requests...</b>")
+    X = "acceser"
+    get = await app.get_messages(chat_id=X, message_ids=3)
+    getint = int(get.text)
+    await app.approve_all_chat_join_requests(getint)
+    await msg.reply("<b>Done.</b>")
+
+print("Bot Is Alive..")
+app.run()
